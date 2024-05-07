4c74d15f602bfc31b8a0eb585a94c0f50b22829b
Adva1t More <157694812+adva1tM@users.noreply.github.com>
2024-05-07 20:22:28 +0530

    This commit was made on 7th May 2024 8:23PM IST.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Basic Python Projects
- This repo contains very basic python games. The aim to make this repo was to practice python and to push my first  code in Github. 
+Hello, I am Adavit More from India. I created this repository on May 6, 2024. I am a beginner learning Python. This repository contains very basic Python games. The aim of making this repository was to practice Python and to push my first code on GitHub. If you are also a beginner like me, you can use this code to understand how it works.
+
+First Game - Snake,Water,Gun. 
+ 
---
+++ Snake,Water,Gun.py
@@ -0,0 +1,52 @@
+import random #Random module is an built-in module used to generate psedu-random numbers. 
+
+def GameWin(Computer, You): #Def declares a function "GameWin" 
+
+# An example and use of Conditional expressions. 
+
+    if Computer == You:
+        return None
+    elif Computer =="S": 
+        if You == "W":
+            return False 
+        elif You == "G":
+            return True
+        
+    elif Computer == "W":
+        if You == "G":
+            return False
+        
+        elif You == "S":
+            return True
+    
+    elif Computer == "G" :
+        if You == "S": 
+            return False 
+        elif You == "W":
+            return True
+
+
+# Here I will use random module that I imported in line 1. Random module In this case will chose from 1 to 3. 
+
+print("Computer's Turn: Snake(S) Water(W) or Gun(G)") 
+randomNumber = random.randint(1, 3) # Random Module will choose from 1 to 3. 
+if randomNumber == 1: 
+    Computer = "S"                  # 1 == "S"
+elif randomNumber == 2:
+    Computer = "W"                  # 2 == "W"
+elif randomNumber == 3:             
+    Computer = "G"                  # 3 == 'G 
+
+You = input("Your Turn: Choose Snake(S) Water(R) or Gun(G): ")
+a = GameWin(Computer, You)
+
+print(F'Computer chose {Computer}')
+print(f'You chose {You}')
+
+if a == None:
+    print("The game is tied!")
+elif a:
+    print("You Won!🥳")
+else: 
+    print("You Lose!😔")
+
